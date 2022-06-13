39e767f9e3afc026e0aba1e5255175d58feb73f5
Nizyi <tomysen812@gmail.com>
2022-06-13 18:27:49 +0200

    13/06/2022 | 18:27:49,14

--- start.bat
+++ start.bat
@@ -4,7 +4,7 @@ git pull
 git push
 pause
 
-java -Xmx2000M -Xms1300M -jar forge-1.16.5-36.2.34.jar 
+java -Xmx1500M -Xms1300M -jar forge-1.16.5-36.2.34.jar 
 
 git add .
 git commit -m "%date% | %time%"
