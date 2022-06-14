af47a5b8d57957b57477780b99c3342533d7bb16
Nizyi <tomysen812@gmail.com>
2022-06-15 01:18:28 +0200

    15/06/2022 |  1:18:28,46

---
+++ world/computercraft/computer/1/startup.lua
@@ -0,0 +1,9 @@
+monitor = peripheral.wrap("left")
+
+monitor.setBackgroundColor(colors.blue)
+monitor.clear()
+
+monitor.setTextColor(colors.black)
+monitor.setTextScale(1.5)
+monitor.clear()
+monitor.write("Regles du serveur")
